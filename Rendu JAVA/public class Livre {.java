0f54671cc5955c6e751718ab81c0143b361e55c3
Dreadpira <156445670+Dreadpira@users.noreply.github.com>
2024-05-15 16:05:53 +0200

    Add files via upload

---
+++ Rendu JAVA/public class Livre {.java
@@ -0,0 +1,19 @@
+public class Livre {
+  public String titre;
+  public String auteur;
+  public String isbn;
+
+  public livre (String titre, String auteur, String isbn) {
+
+  }
+
+  public AfficherDetails(Livre) {
+    return; 
+  }
+
+
+
+}
+
+
+
---
+++ Rendu JAVA/public class Personne {.java
@@ -0,0 +1,23 @@
+public class Personne {
+  public String nom;
+  public String prenom;
+  public int id;
+
+  public Personne(String nom, String prenom, int id) {
+
+  }
+
+  public AfficherDetails(personne) {
+    return; 
+  }
+
+  class Personne(){
+    public date dateAdhesion;
+    public String statut;
+  }
+
+}
+
+
+
+
